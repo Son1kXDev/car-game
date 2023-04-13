683552cd3ebd4ba84d3b4dcfbd2d079b71870993
Son1kX <son1kx@vk.com>
2023-04-14 01:40:09 +0300

    Типо фикс облаков

--- Assets/Game/Scripts/Game/CloudsMovement.cs
+++ Assets/Game/Scripts/Game/CloudsMovement.cs
@@ -1,15 +1,13 @@
-﻿using System.Collections;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Assets.Game.Scripts.Game
 {
     public class CloudsMovement : MonoBehaviour
     {
         [SerializeField, Range(0.1f, 20f)] private float _speed = 1f;
-        [SerializeField, Range(1, 5)] private float _spawnRange = 1f;
+        [SerializeField, Range(0, 5)] private float _spawnRange = 1f;
         [SerializeField] private Camera _skyCamera;
 
-        private float _skyOffset = 20f;
         private Vector2 _offset;
         private bool _hasCopy = false;
 
@@ -26,19 +24,19 @@ namespace Assets.Game.Scripts.Game
             Vector2 screenPos = _skyCamera.WorldToScreenPoint(transform.position);
 
             Vector2 min = screenPos - _offset;
-            Vector2 max = screenPos + _offset;
 
-            if (!_hasCopy && (min.x < 0))
-            {
-                _hasCopy = true;
-                Vector2 spawnPosition = _skyCamera.ScreenToWorldPoint(new Vector3(Screen.width, screenPos.y, 0));
-                spawnPosition.x += _skyOffset;
-                spawnPosition.y += Random.Range(-_spawnRange, _spawnRange);
-                GameObject cloud = Instantiate(gameObject, spawnPosition, Quaternion.identity, transform.parent);
-                cloud.name = "Cloud";
-            }
+            // if (!_hasCopy && (min.x < 0))
+            // {
+            //     _hasCopy = true;
+            //     Vector2 spawnPosition = _skyCamera.ScreenToWorldPoint(new Vector3(Screen.width * 1.5f, screenPos.y, 0));
+            //     spawnPosition.y += Random.Range(-_spawnRange, _spawnRange);
+            //     GameObject cloud = Instantiate(gameObject, spawnPosition, Quaternion.identity, transform.parent);
+            //     cloud.name = "Cloud";
+            // }
 
-            if (transform.position.x < -30f) Destroy(gameObject);
+            if (transform.position.x < -24f)
+                transform.position = new(408f, transform.position.y, transform.position.z);
+
         }
     }
 }
