5e4e45ae283418e40a05d34ffe8446b742356954
Son1kX <son1kx@vk.com>
2023-04-17 16:33:26 +0300

    Добавил настройки для изменения громкости звуков.

--- Assets/Game/Scripts/Audio/AudioManager.cs
+++ Assets/Game/Scripts/Audio/AudioManager.cs
@@ -6,14 +6,36 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
-    void Awake()
+    [Header("Volume")]
+    [Range(0, 1)]
+    public float MasterVolume = 1f;
+    [Range(0, 1)]
+    public float MusicVolume = 1f;
+    [Range(0, 1)]
+    public float SFXVolume = 1f;
+
+    private Bus _masterBus, _musicBus, _sfxBus;
+
+    private void Awake()
     {
         if (Instance) Destroy(this.gameObject);
         else
         {
+            transform.parent = null;
             Instance = this;
             DontDestroyOnLoad(this.gameObject);
         }
+
+        _masterBus = RuntimeManager.GetBus("bus:/");
+        _musicBus = RuntimeManager.GetBus("bus:/Music");
+        _sfxBus = RuntimeManager.GetBus("bus:/SFX");
+    }
+
+    private void Update()
+    {
+        _masterBus.setVolume(MasterVolume);
+        _musicBus.setVolume(MusicVolume);
+        _sfxBus.setVolume(SFXVolume);
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPosition)
---
+++ Assets/Game/Scripts/Audio/VolumeUIControll.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeUIControll : MonoBehaviour
+{
+    [SerializeField] private Slider _masterVolumeSlider;
+    [SerializeField] private Slider _musicVolumeSlider;
+    [SerializeField] private Slider _sfxVolumeSlider;
+
+    void Awake()
+    {
+        _masterVolumeSlider.value = AudioManager.Instance.MasterVolume;
+        _musicVolumeSlider.value = AudioManager.Instance.MusicVolume;
+        _sfxVolumeSlider.value = AudioManager.Instance.SFXVolume;
+    }
+
+    public void UpdateMaster() => AudioManager.Instance.MasterVolume = _masterVolumeSlider.value;
+
+    public void UpdateMusic() => AudioManager.Instance.MusicVolume = _musicVolumeSlider.value;
+
+    public void UpdateSFX() => AudioManager.Instance.SFXVolume = _sfxVolumeSlider.value;
+
+}
