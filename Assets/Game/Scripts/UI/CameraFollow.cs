2844a4a83cec97140e19050c4ed461b0a89cf177
Son1kX <son1kx@vk.com>
2023-05-18 03:50:47 +0300

    Новая виньетка, сделанная с помощью Post Processing Volume.

---
+++ Assets/Game/Scripts/UI/CameraFollow.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using NTC.Global.Cache;
+
+namespace Assets.Game.Scripts.UI
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraFollow : MonoCache
+    {
+        [SerializeField] private Camera _target;
+        private Camera _camera;
+
+        private void Awake() => _camera = GetComponent<Camera>();
+
+        protected override void LateRun()
+        {
+            transform.position = _target.transform.position;
+            _camera.orthographicSize = _target.orthographicSize;
+            _camera.orthographic = _target.orthographic;
+        }
+    }
+}
